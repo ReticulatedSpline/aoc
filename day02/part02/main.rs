abe326e04235a7e287814bc7c954d0900654edcb
Benjamin <carpenter.ben@icloud.com>
2024-02-03 10:56:35 -0600

    completed pt 2

--- day02/part02/main.rs
+++ day02/part02/main.rs
@@ -1,13 +1,14 @@
 use std::fs;
 
 #[derive(Default)]
+#[derive(Debug)]
 struct CubeSet {
-    red: u8,
-    green: u8,
-    blue: u8
+    red: u32,
+    green: u32,
+    blue: u32
 }
 
-fn get_num_from_game_string(input: &str) -> u8 {
+fn get_num_from_game_string(input: &str) -> u32 {
     let mut output = "".to_string();
     for c in input.chars() {
         if !c.is_digit(10) {
@@ -16,33 +17,32 @@ fn get_num_from_game_string(input: &str) -> u8 {
         output.push(c);
     }
     
-    output.parse::<u8>().unwrap()
+    output.parse::<u32>().unwrap()
 }
 
-fn get_min_cube_set(game_string: &str) -> CubeSet {
-    let mut game_string = game_string.split(';');
-    while game_string.is_some() {
-
+fn get_min_cube_set(round_strings: &str) -> CubeSet {
+    let games = round_strings.split(';');
     let mut cube_min_set = CubeSet {..Default::default()};
-        for cube_count_str in &cube_set {
-        println!("{:?}", &cube_set);
-        let cube_num = get_num_from_game_string(cube_count_str);
-        if cube_count_str.contains("red") && cube_num > cube_min_set.red {
-            cube_min_set.red = cube_num;
+    for game in games {
+        println!("{:?}", &game);
+        let cubes = game.split(',');
+        for cube in cubes {
+            let cube_num = get_num_from_game_string(cube);
+            if cube.contains("red") && cube_num > cube_min_set.red {
+                cube_min_set.red = cube_num;
+            }
+            else if cube.contains("blue") && cube_num > cube_min_set.blue {
+                cube_min_set.blue = cube_num
+            }
+            else if cube.contains("green") && cube_num > cube_min_set.green {
+                cube_min_set.green = cube_num
+            } 
         }
-        else if cube_count_str.contains("blue") && cube_num > cube_min_set.blue {
-            cube_min_set.blue = cube_num
-        }
-        else if cube_count_str.contains("green") && cube_num > cube_min_set.green {
-            cube_min_set.green = cube_num
-        } 
-        cube_sets.next()
-    }
     }
     return cube_min_set;
 }
 
-fn get_set_power(cube_set: &CubeSet) -> u32 {
+fn get_set_power(set: &CubeSet) -> u32 {
     set.red * set.blue * set.green
 }
 
@@ -56,13 +56,13 @@ fn main() {
         println!("Game {index_num}:");
         
         // discard header data on each line
-        let game_data = line.split(':').expect("DEBUG")[1];
+        let game_data = line.split(':').collect::<Vec<&str>>()[1];
 
         // iterate through each game, storing largest cube counts
         let min_cube_set = get_min_cube_set(game_data);
-        sum_of_powers += get_set_power(cube_min_set);
+        sum_of_powers += get_set_power(&min_cube_set);
+        println!("{:?}", min_cube_set);
         println!("Running total: {sum_of_powers}\n");
         index_num += 1;
     }
-    println!("{sum_of_powers}");
 }
