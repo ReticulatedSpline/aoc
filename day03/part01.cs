548513be0a2685b6d8a470e51a46f480b520a019
Benjamin <carpenter.ben@icloud.com>
2024-11-23 21:20:54 -0600

    day03: correct handling of line ends

--- day03/part01.cs
+++ day03/part01.cs
@@ -1,6 +1,3 @@
-using System.IO;
-using System.Security.Cryptography.X509Certificates;
-
 class Program
 {
     private static char[,] readFile(string inputFile) {
@@ -39,32 +36,34 @@ class Program
         int colSearchEnd = (endCol < dimensions - 1) ? endCol + 1 : dimensions - 1;
         int rowSearchStart = (row > 0) ? row - 1 : 0;
         int rowSearchEnd = (row < dimensions - 1) ? row + 1 : dimensions - 1;
-        Console.WriteLine($"Checking columns {colSearchStart}-{colSearchEnd}, rows {rowSearchStart}-{rowSearchEnd} for symbols...");
+        //Console.WriteLine($"Checking columns {colSearchStart}-{colSearchEnd}, rows {rowSearchStart}-{rowSearchEnd} for symbols...");
         for (int x = colSearchStart; x <= colSearchEnd; x++) {
             for (int y = rowSearchStart; y <= rowSearchEnd; y++) {
                 if (isSymbol(array[y, x])) {
-                    Console.WriteLine($"Found symbol '{array[y, x]}' at adjacent location {y}, {x}");
+                    //Console.WriteLine($"Found symbol '{array[y, x]}' at adjacent location {y}, {x}");
                     return true;
                 }
             } 
         }
-        Console.WriteLine("Not considering this a part number.\n");
+        //Console.WriteLine("Not considering this a part number.\n");
         return false;
     }
 
     static void Main(string[] args)
     {
+        Console.WriteLine("Running...");
         // read file into 2d array
-        char[,] input = readFile("example.dat");
+        char[,] input = readFile("input.dat");
         int dimension = input.GetLength(0);
         int programSum = 0;
         int startY = 0;
         for (int x = 0; x < dimension; x++) {
-            // consume each line by char
             string numString = "";
             bool readingNum = false;
+            
+            // consume each line by char
             for (int y = 0; y < dimension; y++) {
-                int possiblePartNum;
+                
                 // if char is num, add to potential number string
                 if (char.IsDigit(input[x,y])) {
                     numString += input[x,y];
@@ -72,16 +71,18 @@ class Program
                         startY = y;
                     } 
                     readingNum = true;
-                // if char is period, reset vars, parse number string
-                } else if (!char.IsDigit(input[x,y]) && !string.IsNullOrEmpty(numString)) {
-                    possiblePartNum = int.Parse(numString);
-                    Console.WriteLine($"Found number string '{numString}' in row {x}, columns {startY}-{y-1}");
+                }
+                
+                // if we have a number substring and it ends, reset vars, parse number string
+                if (!string.IsNullOrEmpty(numString) && ((y == dimension - 1) ||!char.IsDigit(input[x,y]))) {
+                    int possiblePartNum = int.Parse(numString);
+                    //Console.WriteLine($"Found number string '{numString}' in row {x}, columns {startY}-{y-1}");
                     // check for adjacent symbols
                     if (isPartNumber(input, startY, y-1, x, dimension)) {
                         // if symbol exists, add to program sum
-                        Console.WriteLine("Considering this a part number!");
+                        //Console.WriteLine("Considering this a part number!");
                         programSum += possiblePartNum;
-                        Console.WriteLine($"Running sum: {programSum}\n");
+                        //Console.WriteLine($"Running sum: {programSum}\n");
                     }
                     startY = 0;
                     numString = "";
