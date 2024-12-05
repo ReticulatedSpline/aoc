8c297f4186d5c693e74b6f0c3d0c516b9e33203c
Benjamin <carpenter.ben@icloud.com>
2024-12-04 18:50:59 -0600

    day03 part01

---
+++ 2024/day03/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.RegularExpressions;
+
+static string readFile(string filePath) {
+    try
+    {
+        using StreamReader reader = new(filePath);
+        return reader.ReadToEnd();
+    }
+    catch (IOException e)
+    {
+        Console.WriteLine("Input could not be read:");
+        Console.WriteLine(e.Message);
+        return "";
+    }
+}
+
+string pattern = @"mul\((\d+),(\d+)\)";
+string input = readFile("input.dat");
+string partTwoInput = new string(input);
+
+int partOneSum = 0;
+int partTwoSum = 0;
+
+// for part 1, just sum
+foreach (Match match in Regex.Matches(input, pattern)) {
+    partOneSum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+}
+
+// for part two, trim no-ops...
+string opPattern = @"don't\(\).*do()";
+foreach (Match match in Regex.Matches(input, opPattern)) {
+    
+}
+
+// then sum
+foreach (Match match in Regex.Matches(input, pattern)) {
+    partTwoSum += int.Parse(match.Groups[1].Value) * int.Parse(match.Groups[2].Value);
+}
+
+Console.WriteLine($"Part 1: {partOneSum}");
+Console.WriteLine($"Part 2: {partTwoSum}");
