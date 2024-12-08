1054464664d8e33e0d1fd1a53491097ce9487331
Ben Carpenter <carpenter.ben@icloud.com>
2024-12-08 13:54:10 -0600

    day05 start

---
+++ 2024/day05/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.RegularExpressions;
+
+static (List<(int,int)>, List<List<int>>) readFile(string filePath) {
+    using (StreamReader sr = new StreamReader(filePath)) {
+        string? line;
+        List<(int, int)> ruleList = new List<(int, int)>();
+        List<List<int>> pageList = new List<List<int>>();
+
+        line = sr.ReadLine();
+        while (line != "")
+        {
+            MatchCollection numbers = Regex.Matches(line, @"\d+");
+            ruleList.Add(numbers[0].Value, numbers[1].Value);
+            line = sr.ReadLine();
+        }
+
+        return (ruleList, pageList);
+    }
+}
+
+int partOneSum = 0;
+int partTwoSum = 0;
+if (args.Length == 0) {
+    Console.WriteLine("Missing input file argument!");
+    return;
+}
+var rules, pages = readFile(args[0]);
+
+Console.WriteLine($"Part One: {partOneSum}");
+Console.WriteLine($"Part Two: {partTwoSum}");
