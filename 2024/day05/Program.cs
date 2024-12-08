9d42f512f0c433b7b252a6499e023fd8d7433da2
Benjamin <carpenter.ben@icloud.com>
2024-12-08 15:13:28 -0600

    day05 part01

--- 2024/day05/Program.cs
+++ 2024/day05/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Formats.Tar;
+using System.Text.RegularExpressions;
 
 static (List<(int,int)>, List<List<int>>) readFile(string filePath) {
     using (StreamReader sr = new StreamReader(filePath)) {
@@ -6,16 +7,36 @@ static (List<(int,int)>, List<List<int>>) readFile(string filePath) {
         List<(int, int)> ruleList = new List<(int, int)>();
         List<List<int>> pageList = new List<List<int>>();
 
-        line = sr.ReadLine();
-        while (line != "")
+        while ((line = sr.ReadLine()) != null &&
+               !string.IsNullOrWhiteSpace(line))
         {
             MatchCollection numbers = Regex.Matches(line, @"\d+");
-            ruleList.Add(numbers[0].Value, numbers[1].Value);
-            line = sr.ReadLine();
+            ruleList.Add((int.Parse(numbers[0].Value),
+                          int.Parse(numbers[1].Value)));
+        }
+
+        while ((line = sr.ReadLine()) != null)
+        {
+            MatchCollection numbers = Regex.Matches(line, @"\d+");
+            List<int> pages = new List<int>();
+            foreach (Match number in numbers) {
+                pages.Add(int.Parse(number.Value));
+            }
+            pageList.Add(pages);
         }
 
         return (ruleList, pageList);
     }
+}
+
+static bool isValidPageSet(List<(int,int)> rules, List<int> pageSet) {
+    foreach (var rule in rules) {
+        if (pageSet.IndexOf(rule.Item1) >= 0 && pageSet.IndexOf(rule.Item2) >= 0 &&
+            pageSet.IndexOf(rule.Item1) > pageSet.IndexOf(rule.Item2)) {
+            return false;
+        } 
+    }
+    return true;
 }
 
 int partOneSum = 0;
@@ -24,7 +45,21 @@ if (args.Length == 0) {
     Console.WriteLine("Missing input file argument!");
     return;
 }
-var rules, pages = readFile(args[0]);
+
+var (rules, pageDir) = readFile(args[0]);
+bool isValid;
+
+foreach (var pageSet in pageDir) {
+    isValid = true;
+    foreach (var page in pageSet) {
+        if (!isValidPageSet(rules, pageSet)) {
+            isValid = false;
+        } 
+    }
+    if (isValid) {
+        partOneSum += pageSet[pageSet.Count / 2];
+    }
+}
 
 Console.WriteLine($"Part One: {partOneSum}");
 Console.WriteLine($"Part Two: {partTwoSum}");
