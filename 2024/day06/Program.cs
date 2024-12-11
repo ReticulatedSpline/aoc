7d752e2340178e70d668e83512c4a67f8b870e72
Benjamin <carpenter.ben@icloud.com>
2024-12-10 21:33:55 -0600

    day06 part01

--- 2024/day06/Program.cs
+++ 2024/day06/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Data;
-
-static (char[,], (uint X, uint Y)) readFile(string filePath) {
+﻿static (char[,], (uint X, uint Y)) readFile(string filePath) {
     using (StreamReader sr = new StreamReader(filePath)) {
         string? line = sr.ReadLine();
         var loc = (0u,0u);
@@ -27,47 +25,54 @@ static (char[,], (uint X, uint Y)) readFile(string filePath) {
 }
 
 static (Direction, (uint X, uint Y)) advancePosition(char[,] map, (uint X, uint Y) loc, Direction dir) {
-    if (map[loc.X, loc.Y] != '#') {
-        switch (dir) {
-            case Direction.Up:
-                loc = (loc.X, loc.Y - 1);
-                break;
-            case Direction.Right:
-                loc = (loc.X + 1, loc.Y);
-                break;
-            case Direction.Down:
-                loc = (loc.X, loc.Y + 1);
-                break;
-            case Direction.Left:
-                loc = (loc.X - 1, loc.Y);
-                break;
-        }
-    } else {
+    var nextLoc = loc;
+    switch (dir) {
+        case Direction.Up:
+            nextLoc = (loc.X - 1, loc.Y);
+            break;
+        case Direction.Right:
+            nextLoc = (loc.X, loc.Y + 1);
+            break;
+        case Direction.Down:
+            nextLoc = (loc.X + 1, loc.Y);
+            break;
+        case Direction.Left:
+            nextLoc = (loc.X, loc.Y - 1);
+            break;
+    }
+
+    char nextChar = map[nextLoc.X, nextLoc.Y];
+
+    if (nextChar == '#') {
         switch (dir) {
             case Direction.Up:
                 dir = Direction.Right;
-                loc = (loc.X + 1, loc.Y);
                 break;
             case Direction.Right:
                 dir = Direction.Down;
-                loc = (loc.X, loc.Y + 1);
                 break;
             case Direction.Down:
                 dir = Direction.Left;
-                loc = (loc.X - 1, loc.Y);
                 break;
             case Direction.Left:
                 dir = Direction.Up;
-                loc = (loc.X, loc.Y - 1);
                 break;
         }
+    } else {
+        loc = nextLoc;
     }
-
     return (dir, loc);
 }
 
-int partOneSum = 0;
-//int partTwoSum = 0;
+static uint countPositions(char[,] map) {
+    uint count = 0;
+    for(int i = 0; i < map.GetLength(0); i++) {
+       for(int k = 0; k < map.GetLength(0); k++) {
+            if (map[i,k] == 'X') count++;
+        }
+    }
+    return count;
+}
 
 if (args.Length == 0) {
     Console.WriteLine("Missing input file argument!");
@@ -75,15 +80,18 @@ if (args.Length == 0) {
 }
 
 var (map, loc) = readFile(args[0]);
-var dim = map.GetLength(0);
 var dir = Direction.Up;
-do {
-    (dir, loc) = advancePosition(map, loc, dir);
-    partOneSum++;
-} while (loc.X >= 0 && loc.Y >= 0 &&
-         loc.X < dim && loc.Y < dim);
 
-Console.WriteLine($"Part one: {partOneSum}");
+// dont make bully me for this
+try {
+    do {
+        map[loc.X, loc.Y] = 'X';
+        (dir, loc) = advancePosition(map, loc, dir);
+        map[loc.X, loc.Y] = '^';
+    } while (true);
+} catch (System.IndexOutOfRangeException) {
+    Console.WriteLine($"Part one: {countPositions(map)}");
+}
 
 enum Direction {
     Up,
