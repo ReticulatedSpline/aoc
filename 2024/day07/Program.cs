4d741859ec1b0aefd289a417ac26628a5d81bac6
Benjamin <carpenter.ben@icloud.com>
2024-12-23 15:12:37 -0600

    Didn't get as far as I'd like but an improvement over last year. See you at the next one...

--- 2024/day07/Program.cs
+++ 2024/day07/Program.cs
@@ -1,9 +1,4 @@
-﻿using System.Diagnostics.CodeAnalysis;
-using System.Numerics;
-using System.Reflection.Emit;
-using System.Runtime.CompilerServices;
-
-static List<(long, List<int>)> readFile(string filePath) {
+﻿static List<(long, List<int>)> readFile(string filePath) {
     using (StreamReader sr = new StreamReader(filePath)) {
         string? line;
         var inputModel = new List<(long, List<int>)>();
@@ -25,31 +20,29 @@ static List<(long, List<int>)> readFile(string filePath) {
 
 static bool isPossibleEquation(long result, int currentSum, List<char> operators, List<int> operands) {
     
-    Console.WriteLine($"Recursion level: {operands.Count} operands left.");
     if (operands.Count == 0) {
-        Console.WriteLine($"Out of operands. Returning {currentSum} == {result}.");
         return currentSum == result;
     } else if (currentSum > result) {
-        Console.WriteLine($"Current sum of {currentSum} has surpassed {result}.");
         return false;
     }
 
+    int currentOperand = operands[0];
+    List<int> remainingOperands = operands.Skip(1).ToList();
+    
     foreach(var op in operators) {
         bool found = false;
-        List<int> remainingOperands = operands.Skip(1).ToList();
         switch (op) {
             case '+':
-                found = isPossibleEquation(result, currentSum + operands[0], operators, remainingOperands);
+                found = isPossibleEquation(result, currentSum + currentOperand, operators, remainingOperands);
                 break;
             case '*':
-                found = isPossibleEquation(result, currentSum * operands[0], operators, remainingOperands);
+                found = isPossibleEquation(result, currentSum * currentOperand, operators, remainingOperands);
                 break;
             default:
                 throw new InvalidOperationException("Unsupported operator");
         }
 
         if (found) {
-            Console.WriteLine("Possible equation.");
             return true;
         }
     }
@@ -65,13 +58,12 @@ if (args.Length == 0) {
 var equations = readFile(args[0]);
 long partOneSum = 0;
 
+
 foreach ((long result, List<int> operands) in equations) {
     List<char> operators = ['*', '+'];
-
-    Console.WriteLine();
-    Console.Write($"Checking equation {result}: ");
-    foreach(var operand in operands) {
-        Console.Write($"{operand}, ");
+    Console.Write($"\nChecking equation {result}: ");
+    foreach (var op in operands) {
+        Console.Write($"{op}, ");
     }
     Console.WriteLine();
 
